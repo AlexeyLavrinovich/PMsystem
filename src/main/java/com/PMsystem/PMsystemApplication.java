df5ed11b50f97b47beba0e0571b748c3c0eca7ee
Alexey Lavrinovich <lavrinleha95@gmail.com>
2022-04-23 17:12:51 +0300

    first commit

---
+++ src/main/java/com/PMsystem/PMsystemApplication.java
@@ -0,0 +1,13 @@
+package com.PMsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PMsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PMsystemApplication.class, args);
+	}
+
+}
