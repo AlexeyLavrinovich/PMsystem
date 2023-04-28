6732408b7e263079035b13112daa3b27497e8a33
Alexey Lavrinovich <lavrinleha95@gmail.com>
2023-04-28 02:24:14 -0700

    Add registration and error pages

--- src/main/java/com/PMsystem/controller/GlobalExceptionHandler.java
+++ src/main/java/com/PMsystem/controller/GlobalExceptionHandler.java
@@ -3,7 +3,7 @@ package com.PMsystem.controller;
 import com.PMsystem.exception.AlreadyExistsException;
 import com.PMsystem.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -13,20 +13,32 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(AlreadyExistsException.class)
-    public ResponseEntity<String> handleAlreadyExistsException(AlreadyExistsException alreadyExistsException) {
+    public String handleAlreadyExistsException(
+            AlreadyExistsException alreadyExistsException,
+            Model model
+    ) {
         log.error("AlreadyExistsException: {}", alreadyExistsException.getMessage());
-        return ResponseEntity.badRequest().body(alreadyExistsException.getMessage());
+        model.addAttribute("error", alreadyExistsException.getMessage());
+        return "error";
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleNotFoundException(NotFoundException notFoundException) {
+    public String handleNotFoundException(
+            NotFoundException notFoundException,
+            Model model
+    ) {
         log.error("NotFoundException: {}", notFoundException.getMessage());
-        return ResponseEntity.badRequest().body(notFoundException.getMessage());
+        model.addAttribute("error", notFoundException.getMessage());
+        return "error";
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception exception) {
+    public String handleException(
+            Exception exception,
+            Model model
+    ) {
         log.error("Exception: {}", exception.getMessage());
-        return ResponseEntity.badRequest().body("Something bad happened...");
+        model.addAttribute("error", exception.getMessage());
+        return "error";
     }
 }
--- src/main/java/com/PMsystem/controller/RegistrationController.java
+++ src/main/java/com/PMsystem/controller/RegistrationController.java
@@ -1,28 +1,40 @@
 package com.PMsystem.controller;
 
+import com.PMsystem.entity.ProjectEntity;
 import com.PMsystem.entity.UserEntity;
 import com.PMsystem.exception.AlreadyExistsException;
 import com.PMsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/registration")
+@Controller
+@RequestMapping("/users/registration")
 public class RegistrationController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity info() {
-        return ResponseEntity.ok("Set your name and password");
+    public String info(Model model) {
+        UserEntity user = new UserEntity();
+        model.addAttribute("user", user);
+        return "registration";
     }
 
     @PostMapping
-    public ResponseEntity registration(@RequestBody UserEntity user) throws AlreadyExistsException {
+    public String registration(
+            @ModelAttribute UserEntity user,
+            BindingResult result
+    ) throws AlreadyExistsException {
+        if (result.hasErrors()) {
+            return "registration";
+        }
         userService.addUser(user);
-        return ResponseEntity.ok("User was successfully add!");
+        return "redirect:/users";
     }
 
 }
